04fe6fbe81216e516ea755fa6ee14c079ffd61d0
sarvesh <sarvesh@ecaplabs.com>
2025-11-15 20:30:10 +0530

    react version updated and next config changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // Forces Next to use Webpack instead of Turbopack for build & prod
+  webpack(config, { isServer }) {
+    // No modifications needed unless you have custom loaders
+    return config;
+  },
 
-module.exports = nextConfig
+  // Transpile Chakra's packages so they're bundled properly
+  transpilePackages: [
+    "@chakra-ui/react",
+    "@chakra-ui/utils",
+    "@chakra-ui/system",
+  ],
+
+  // Fallback: use the Webpack flag in npm scripts
+  // (See below)
+};
+
+module.exports = nextConfig;
